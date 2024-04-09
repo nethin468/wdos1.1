d09eeac8b0225d42f93ec21ee00547e3b27edb09
nethin468 <nethinpanapitiya@gmail.com>
2024-04-09 16:51:46 +0530

    5th commit

--- dashboard.js
+++ dashboard.js
@@ -1,15 +1,12 @@
-// Retrieve the array from local storage
 const subscriptionsString = localStorage.getItem('newsletterSubscriptions');
 
-// Check if there are subscriptions stored in local storage
+
 if (subscriptionsString) {
-    // Parse the JSON string into an array
+   
     const subscriptions = JSON.parse(subscriptionsString);
-    
-    // Get the textarea element
+
     const textarea = document.getElementById('newsletterTextarea');
 
-    // Set the value of the textarea to the subscriptions array joined by newline characters
     textarea.value = subscriptions.join('\n');
 } else {
     console.log('No newsletter subscriptions found in local storage.');
